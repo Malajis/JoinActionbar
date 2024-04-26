6f88e2e218c5de674c5e21f345575ab09b32a443
Malajis <625818408@qq.com>
2024-04-26 22:40:29 +0000

    update BetterAccess/src/main/java/me/malajis/betteraccess/Command.java. 删除多余代码
    Signed-off-by: Malajis <625818408@qq.com>

--- BetterAccess/src/main/java/me/malajis/betteraccess/Command.java
+++ BetterAccess/src/main/java/me/malajis/betteraccess/Command.java
@@ -8,9 +8,6 @@ public class Command implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command, String s, String[] strings) {
         Main.onEnable.reloadConfig();
-        String reset = "\u001B[0m";
-        String yellow = "\u001B[33m";
-        System.out.println(yellow+"BetterAccess已经重载成功！"+reset);
         commandSender.sendMessage(Main.onEnable.getConfig().getString("Message.reload").replace("&","§"));
         return false;
     }
