04c3dfbd45c4cb59f1a68ba7c745d1602379087f
Malajis <625818408@qq.com>
2024-04-27 17:32:00 +0800

    UpdataAgain

---
+++ src/main/java/me/malajis/betteraccess/AccessListener.java
@@ -0,0 +1,43 @@
+package me.malajis.betteraccess;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class AccessListener implements Listener {
+    //进入服务器触发事件
+    @EventHandler
+    public void onJoin(PlayerJoinEvent j) {
+        j.setJoinMessage(null);
+        Player p = j.getPlayer();
+        String message = "JoinMessage";
+        if (p.hasPermission("betteraccess.op")) {
+            message = "JoinMessageOP";
+        }else if(p.hasPermission("betteraccess.vip")){
+            message = "JoinMessageVIP";
+        }
+        for (Player online : Bukkit.getOnlinePlayers()) {
+            online.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(Main.onEnable.getConfig().getString(message).replace("%player%", p.getName()).replace("&", "§")));
+        }
+    }
+    //退出服务器触发事件
+    @EventHandler
+    public void onQuit(PlayerQuitEvent q){
+        q.setQuitMessage(null);
+        Player p = q.getPlayer();
+        String message = "QuitMessage";
+        if (p.hasPermission("betteraccess.op")) {
+            message = "QuitMessageOP";
+        }else if(p.hasPermission("betteraccess.vip")){
+            message = "QuitMessageVIP";
+        }
+        for (Player online : Bukkit.getOnlinePlayers()) {
+            online.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(Main.onEnable.getConfig().getString(message).replace("%player%", p.getName()).replace("&", "§")));
+        }
+    }
+}
---
+++ src/main/java/me/malajis/betteraccess/Main.java
@@ -0,0 +1,29 @@
+package me.malajis.betteraccess;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    static Main onEnable;
+    @Override
+    public void onEnable() {
+        String reset = "\u001B[0m";
+        String yellow = "\u001B[33m";
+        //启动文本
+        System.out.println(yellow+"BetterAccess 已经成功加载至服务器"+reset);
+        //注册事件监听器
+        Bukkit.getPluginManager().registerEvents(new AccessListener(),this);
+        //注册命令类
+        Bukkit.getPluginCommand("bareload").setExecutor(new Command());
+        //生成配置文件
+        saveDefaultConfig();
+        onEnable=this;
+    }
+    @Override
+    public void onDisable() {
+        String reset = "\u001B[0m";
+        String yellow = "\u001B[33m";
+        //关闭文本
+        System.out.println(yellow+"BetterAccess 已经成功从服务器中卸载"+reset);
+    }
+}
