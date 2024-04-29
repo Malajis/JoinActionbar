9379e1f873aebb27905ed878bc14e5002fa0f3b6
Malajis <625818408@qq.com>
2024-04-29 08:17:24 +0800

    添加注释

--- src/main/java/me/malajis/betteraccess/AccessListener.java
+++ src/main/java/me/malajis/betteraccess/AccessListener.java
@@ -15,14 +15,17 @@ public class AccessListener implements Listener {
     //进入服务器触发事件
     @EventHandler
     public void onJoin(PlayerJoinEvent j) {
+        //取消原版提示
         j.setJoinMessage(null);
         Player p = j.getPlayer();
         String message = "JoinMessage";
+        //判断权限对应提示文本
         if (p.hasPermission("betteraccess.op")) {
             message = "JoinMessageOP";
         }else if(p.hasPermission("betteraccess.vip")){
             message = "JoinMessageVIP";
         }
+        //给所有在线玩家发送提示
         for (Player online : Bukkit.getOnlinePlayers()) {
             online.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(Objects.requireNonNull(Main.onEnable.getConfig().getString(message)).replace("%player%", p.getName()).replace("&", "§")));
         }
@@ -30,14 +33,17 @@ public class AccessListener implements Listener {
     //退出服务器触发事件
     @EventHandler
     public void onQuit(PlayerQuitEvent q){
+        //取消原版提示
         q.setQuitMessage(null);
         Player p = q.getPlayer();
         String message = "QuitMessage";
+        //判断权限对应提示文本
         if (p.hasPermission("betteraccess.op")) {
             message = "QuitMessageOP";
         }else if(p.hasPermission("betteraccess.vip")){
             message = "QuitMessageVIP";
         }
+        //给所有在线玩家发送提示
         for (Player online : Bukkit.getOnlinePlayers()) {
             online.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(Objects.requireNonNull(Main.onEnable.getConfig().getString(message)).replace("%player%", p.getName()).replace("&", "§")));
         }
